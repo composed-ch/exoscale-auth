f01bd13679e155d38dcbab1a9ec6c67f6b08572f
Patrick Bucher <patrick.bucher@mailbox.org>
2025-04-07 13:29:03 +0200

    provide repo url

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject com.github.composed-ch/exoscale-auth "0.1.0-SNAPSHOT"
   :description "Exoscale API Request Signature"
-  :url "http://example.com/FIXME"
+  :url "https://github.com/composed-ch/exoscale-auth"
   :license {:name "LGPL-3.0-or-later"
             :url "https://www.gnu.org/licenses/lgpl+gpl-3.0.txt"}
   :dependencies [[org.clojure/clojure "1.11.1"]
